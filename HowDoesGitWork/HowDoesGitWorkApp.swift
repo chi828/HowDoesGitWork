09ef82394c5e272ff1fdd950c14008846d2f5005
chi828 <124188695+chi828@users.noreply.github.com>
2023-11-17 14:33:14 +0100

    Initial Commit

---
+++ HowDoesGitWork/HowDoesGitWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowDoesGitWorkApp.swift
+//  HowDoesGitWork
+//
+//  Created by Chiara Giorgia Ricci on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HowDoesGitWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
